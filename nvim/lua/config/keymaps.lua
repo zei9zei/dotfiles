891b8fc1d94f7273b73e30966fd0fb03128438be
zei9zei <81759131+zei9zei@users.noreply.github.com>
2025-01-23 21:40:33 +0100

    Added keymaps for search and center

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "n", "nzz", { desc = "Next search item and center", silent = true })
+vim.keymap.set("n", "N", "Nzz", { desc = "Previous search item and center", silent = true })
