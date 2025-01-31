98ed1940a89a6bbb66908ad552e60896c2ad923a
zei9zei <81759131+zei9zei@users.noreply.github.com>
2025-01-31 11:17:43 +0100

    From dir to symlink

--- setup.sh
+++ setup.sh
@@ -9,15 +9,8 @@ brew update && for package in "${packages[@]}"; do brew install "$package"; done
 # Crea la cartella .config/nvim
 mkdir -p ~/.config/nvim
 
-# Copia o crea un symlink della cartella nvim
-if [ -d "./nvim" ]; then
-  # Copia la cartella nvim
-  cp -r ./nvim ~/.config/nvim
-  # Oppure crea un symlink
-  # ln -s "$(pwd)/nvim" ~/.config/nvim
-else
-  echo "La cartella 'nvim' non esiste allo stesso livello di setup.sh"
-fi
+# Crea un symlink
+ln -s "$(pwd)/nvim" ~/.config/nvim
 
 # Messaggio di completamento
 echo "Installazione completata!"
